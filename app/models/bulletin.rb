0b4b2950b6fd7239d3bf41c0c4c39f2e146c9611
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-04-11 19:01:55 +0300

    Added hook to capitalize bulletin/category name before save

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -3,6 +3,8 @@
 class Bulletin < ApplicationRecord
   has_one_attached :image
 
+  before_save :capitalize_name
+
   belongs_to :user
   belongs_to :category
 
@@ -12,4 +14,10 @@ class Bulletin < ApplicationRecord
   validates :image, attached: true,
                     content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                     size: { less_than: 5.megabytes }
+
+  private
+
+  def capitalize_name
+    name.capitalize!
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  before_save :capitalize_name
+
   validates :name, presence: true
+
+  private
+
+  def capitalize_name
+    name.capitalize!
+  end
 end
