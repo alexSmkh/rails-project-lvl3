558d519bf23b0abe895407c80c085d0ff8496710
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-04-14 19:20:24 +0300

    Added bulletin controller tests

---
+++ test/controllers/web/bulletins_controller_test.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'securerandom'
+
+class BulletinsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @bulletin = bulletins(:one)
+    @category = categories(:one)
+    @user = users(:one)
+  end
+
+  test 'should get index' do
+    get bulletins_url
+    assert_response :success
+  end
+
+  test 'signed user should get new' do
+    sign_in @user
+    get new_bulletin_url
+    assert_response :success
+  end
+
+  test 'unauth user should not get new' do
+    get new_bulletin_url
+    assert_redirected_to root_path
+  end
+
+  test 'signed user should create bulletin' do
+    sign_in @user
+
+    bulletin = {
+      category_id: @category.id,
+      description: Faker::Lorem.paragraph(sentence_count: 5),
+      title: Faker::Commerce.product_name.capitalize,
+      image: fixture_file_upload('one.png', 'image/png')
+    }
+
+    post bulletins_path, params: { bulletin: bulletin }
+
+    new_bulletin =
+      Bulletin.find_by(
+        title: bulletin[:title],
+        description: bulletin[:description],
+        category_id: @category.id
+      )
+
+    assert_redirected_to bulletin_path(new_bulletin)
+    assert { new_bulletin.image.attached? }
+    assert { new_bulletin }
+  end
+
+  test 'unauth user should not create bulletin' do
+    title = 'Title'
+    post bulletins_path,
+         params: {
+           bulletin: {
+             category_id: @category.id,
+             description: Faker::Lorem.paragraph(sentence_count: 5),
+             title: title,
+             image: fixture_file_upload('one.png', 'image/png')
+           }
+         }
+
+    assert_nil Bulletin.find_by(title: title)
+    assert_redirected_to root_path
+  end
+
+  test 'should show bulletin' do
+    get bulletin_url(@bulletin)
+    assert_response :success
+  end
+
+  test 'signed user should get edit' do
+    sign_in @user
+    get edit_bulletin_url(@bulletin)
+    assert_response :success
+  end
+
+  test 'unauth user should not get edit' do
+    get edit_bulletin_url(@bulletin)
+    assert_redirected_to root_path
+  end
+
+  test 'should update bulletin' do
+    sign_in @user
+
+    updated_description = Faker::Lorem.paragraph(sentence_count: 5)
+    updated_title = Faker::Commerce.product_name.capitalize
+    category_id = categories(:two).id
+
+    patch bulletin_path(@bulletin),
+          params: {
+            bulletin: {
+              category_id: category_id,
+              description: updated_description,
+              title: updated_title
+            }
+          }
+
+    assert_redirected_to bulletin_url(@bulletin)
+
+    @bulletin.reload
+
+    assert { @bulletin.title == updated_title }
+    assert { @bulletin.description == updated_description }
+    assert { @bulletin.category_id == category_id }
+  end
+
+  test 'unauth user should not destroy bulletin' do
+    delete bulletin_url(@bulletin)
+
+    assert { Bulletin.find_by(id: @bulletin.id) }
+    assert_redirected_to root_path
+  end
+
+  test 'signed user should destroy bulletin' do
+    sign_in @user
+    delete bulletin_url(@bulletin)
+
+    assert_nil Bulletin.find_by(id: @bulletin.id)
+    assert_redirected_to bulletins_path
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,9 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-  def sign_in(user, _options)
+  include AuthConcern
+
+  def sign_in(user, _options = {})
     auth_hash = {
       provider: 'github',
       uid: '12345',
@@ -30,13 +32,5 @@ class ActionDispatch::IntegrationTest
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
 
     get callback_auth_url('github')
-  end
-
-  def signed_in?
-    session[:user_id].present? && current_user.present?
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
