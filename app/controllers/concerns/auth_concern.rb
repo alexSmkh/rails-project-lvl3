f7076540c09ec2a1518796d735f9a11cc9e9fabf
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-04-14 19:19:50 +0300

    Added current_user existence condition

--- app/controllers/concerns/auth_concern.rb
+++ app/controllers/concerns/auth_concern.rb
@@ -12,7 +12,7 @@ module AuthConcern
   end
 
   def current_user
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def signed_in?
