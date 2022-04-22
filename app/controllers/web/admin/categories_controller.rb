7c500a0a49f1630346313431ef712249367b127c
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-04-22 13:24:09 +0300

    Added the admin categories

---
+++ app/controllers/web/admin/categories_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Web::Admin::CategoriesController < Web::Admin::ApplicationController
+  before_action :set_nav_categories, except: :destroy
+
+  def index
+    @categories = Category.order(:name)
+  end
+
+  def new
+    @category = Category.new
+    authorize @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    authorize @category
+
+    if @category.save
+      redirect_to admin_categories_path, notice: t('.successfully_created')
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    authorize @category
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    authorize @category
+
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: t('.successfully_updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to admin_categories_path, notice: t('.successfully_destroyed')
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
