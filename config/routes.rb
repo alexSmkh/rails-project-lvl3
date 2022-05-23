42cadb32b2ae438fec9db583d2baf1771c6249c3
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-05-23 11:53:04 +0300

    Fixed session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
-    get 'sessions/new', as: 'new_session'
-    delete 'sessions', to: 'sessions#destroy', as: 'session'
+    resource :session, only: %i[new destroy]
 
     resources :bulletins do
       patch 'archive', on: :member
