365309e11e8439e3e715dec20dd2a634007c15e1
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-05-23 13:28:04 +0300

    Added bulletin has_many relation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :bulletins, dependent: :destroy
+
   validates :email, presence: true
   validates :name, presence: true
 end
